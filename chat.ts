d7ff2851134df5b6fa422bd86c04c82590d3ef06
Saksham Adhikari <tarive22@gmail.com>
2025-11-22 11:20:29 -0600

    Pre Agent Work Commit

---
+++ chat.ts
@@ -0,0 +1,50 @@
+//Chatbot with json schema. 
+import {fileURLToPath} from "url";
+import path from "path";
+import {getLlama, LlamaChatSession, defineChatSessionFunction} from "node-llama-cpp";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const llama = await getLlama();
+const model = await llama.loadModel({
+    modelPath: path.join(process.env.HOME!, ".node-llama-cpp", "models", "hf_Qwen_Qwen3-4B.Q8_0.gguf")
+});
+const context = await model.createContext();
+const session = new LlamaChatSession({
+    contextSequence: context.getSequence()
+});
+
+const fruitPrices: Record<string, string> = {
+    "apple": "$6",
+    "banana": "$4"
+};
+const functions = {
+    getFruitPrice: defineChatSessionFunction({
+        description: "Get the price of a fruit",
+        params: {
+            type: "object",
+            properties: {
+                name: {
+                    type: "string"
+                }
+            }
+        },
+        async handler(params) {
+            const name = params.name.toLowerCase();
+            if (Object.keys(fruitPrices).includes(name))
+                return {
+                    name: name,
+                    price: fruitPrices[name]
+                };
+
+            return `Unrecognized fruit "${params.name}"`;
+        }
+    })
+};
+
+
+const q1 = "Is an apple more expensive than a banana?";
+console.log("User: " + q1);
+
+const a1 = await session.prompt(q1, {functions});
+console.log("AI: " + a1);
